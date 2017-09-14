99a168b35d7518eb161d1c0a1e6ee26d83b8f5df
Samuel Pittman <samuelpittman@Samuels-MacBook-Air.local>
2017-09-13 20:16:45 -0400

    second

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, World!" );
+        System.out.println("Today is September 13, 2017. I am alive!");
+    }
+}
